ffd4765208b8e21d46a05df0d07199a688569f10
Prameya-Hegde <112473331+Prameya-Hegde@users.noreply.github.com>
2025-08-22 10:30:14 +0530

    Update_monitor1.py

--- monitor.py
+++ monitor.py
@@ -1,37 +1,34 @@
-
 from time import sleep
 import sys
 
 
+def check_vitals(temperature, pulseRate, spo2):
+    issues = []
+    if temperature > 102 or temperature < 95:
+        issues.append('Temperature critical!')
+    if pulseRate < 60 or pulseRate > 100:
+        issues.append('Pulse Rate is out of range!')
+    if spo2 < 90:
+        issues.append('Oxygen Saturation out of range!')
+    return issues
+
+
+def show_alert(message):
+    print(message)
+    for _ in range(6):
+        print('\r* ', end='')
+        sys.stdout.flush()
+        sleep(1)
+        print('\r *', end='')
+        sys.stdout.flush()
+        sleep(1)
+    print()  # Move to next line after animation
+
+
+# Main_function
 def vitals_ok(temperature, pulseRate, spo2):
-  if temperature > 102 or temperature < 95:
-    print('Temperature critical!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  elif pulseRate < 60 or pulseRate > 100:
-    print('Pulse Rate is out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  elif spo2 < 90:
-    print('Oxygen Saturation out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  return True
+    issues = check_vitals(temperature, pulseRate, spo2)
+    for issue in issues:
+        show_alert(issue)
+    return len(issues) == 0
+
